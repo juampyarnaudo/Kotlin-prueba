19a6636787214fa76a4be43d7831de6c4c1602be
Juampy <iarnaudojuanpablo@gmail.com>
2020-01-27 19:01:38 -0300

    Funciones nuevas
    Agregue funcionalidad al boton click y KAT

--- app/src/main/java/com/example/kotlinprueba/Activities/ClickEventsActivity.kt
+++ app/src/main/java/com/example/kotlinprueba/Activities/ClickEventsActivity.kt
@@ -2,12 +2,50 @@ package com.example.kotlinprueba.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.kotlinprueba.R
 
-class ClickEventsActivity : AppCompatActivity() {
+class ClickEventsActivity : AppCompatActivity(), View.OnLongClickListener {
+
+//    1) Click en XML
+//    2) Click en linea
+//    3) Click by IDS en When
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_events)
+        clickInLine()
+
+        val btn1 = findViewById<Button>(R.id.buttonClickMulti1)
+        val btn2 = findViewById<Button>(R.id.buttonClickMulti2)
+        val btn3 = findViewById<Button>(R.id.buttonClickMulti3)
+
+        btn1.setOnLongClickListener(this)
+        btn2.setOnLongClickListener(this)
+        btn3.setOnLongClickListener(this)
+    }
+
+
+//    regla obligatoria para clicks en XMLs:
+//    El metodo tiene que ser publico, nombres tienen que coincidir, y recibe un solo parametro(view).
+
+    fun xmlClick(view: View){
+    Toast.makeText(this,"click en XML",Toast.LENGTH_SHORT).show()
+
+    }
+    private fun clickInLine(){
+        val btn = findViewById<Button>(R.id.buttonClickInLine)
+        btn.setOnClickListener{Toast.makeText(this,"Click in Line", Toast.LENGTH_SHORT).show()}
+    }
+
+    override fun onLongClick(view: View): Boolean {
+    when(view.id){
+        R.id.buttonClickMulti1 -> Toast.makeText(this,"Click Multi 1", Toast.LENGTH_SHORT).show()
+        R.id.buttonClickMulti2 -> Toast.makeText(this,"Click Multi 2", Toast.LENGTH_SHORT).show()
+        R.id.buttonClickMulti3 -> Toast.makeText(this,"Click Multi 3", Toast.LENGTH_SHORT).show()
+    }
+        return true
     }
 }
--- app/src/main/java/com/example/kotlinprueba/Activities/KotlinAndroidExtensionsActivity.kt
+++ app/src/main/java/com/example/kotlinprueba/Activities/KotlinAndroidExtensionsActivity.kt
@@ -2,12 +2,22 @@ package com.example.kotlinprueba.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.kotlinprueba.R
+import kotlinx.android.synthetic.main.activity_kotlin_android_extensions.*
+
 
 class KotlinAndroidExtensionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extensions)
+
+        val btn = findViewById<Button>(R.id.buttonById)
+        btn.setOnClickListener{ Toast.makeText(this,"Click by ID",Toast.LENGTH_SHORT).show()}
+
+        buttonByKAT.setOnClickListener{ Toast.makeText(this,"Click by KAT",Toast.LENGTH_SHORT).show()}
+
     }
 }
